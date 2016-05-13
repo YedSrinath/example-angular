081eb33876ebdb7343d6759a13dc456386e20167
whoisandie <rjn143@gmail.com>
2016-05-12 18:58:16 -0700

    start github api integration

--- app/scripts/temperature.js
+++ app/scripts/temperature.js
@@ -56,15 +56,26 @@ angular
     }
   }
 
-function TempCtrl() {
+function TempCtrl($http) {
   var vm = this;
 
   vm.temp = null;
   vm.medianVal = null;
   vm.temperatures = [];
+  vm.github_name = null;
 
   vm.create = create;
   vm.median = median;
+
+  $http({
+    type: 'GET',
+    url: 'http://api.github.com/users/whoisandie'
+  }).then(function(res) {
+    //console.log(res.data.name);
+    vm.github_name = res.data.name;
+  }, function() {
+    console.log(err);
+  });
 
   function reset() {
     vm.temp = '';
